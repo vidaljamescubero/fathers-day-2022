d3bb0b8b7aea3b9f0c69dea2e8dbb8e048573993
vidal james cubero <cubero.vj@gmail.com>
2022-05-20 07:50:21 +0800

    last part nalang

--- script.js
+++ script.js
@@ -11,6 +11,17 @@ window.addEventListener('resize' , function(e) {
         let block2 = this.document.querySelector('.fday-block2');
         block2.style.cssText = ` `
     }
+
+    if(screen.width < 375) {
+        let block3 = document.querySelector('.fday-block3');
+        let width = screen.width;
+        let trueWidth = width / 375;
+        let height = trueWidth * 5880;
+        block3.style.cssText = `width: 100%; height: ${height}px; transform: scale(${trueWidth}); transform-origin: top left;`
+    } else {
+        let block3 = document.querySelector('.fday-block3');
+        block3.style.cssText = ` `
+    }
 })
 
 if (this.screen.width < 1440 && this.screen.width > 700) {
@@ -22,6 +33,17 @@ if (this.screen.width < 1440 && this.screen.width > 700) {
 } else {
     let block2 = this.document.querySelector('.fday-block2');
     block2.style.cssText = ` `
+}
+
+if(screen.width < 375) {
+    let block3 = document.querySelector('.fday-block3');
+    let width = screen.width;
+    let trueWidth = width / 375;
+    let height = trueWidth * 5880;
+    block3.style.cssText = `width: 100%; height: ${height}px; transform: scale(${trueWidth}); transform-origin: top left;`
+} else {
+    let block3 = document.querySelector('.fday-block3');
+    block3.style.cssText = ` `
 }
 
 // quotes
@@ -36,6 +58,13 @@ let quotefunction= function() {
         document.querySelector('.fday-block2-3-dot-1').style.cssText = `background: #000`
         document.querySelector('.fday-block2-3-dot-2').style.cssText = `background: #C4C4C4`
         document.querySelector('.fday-block2-3-dot-3').style.cssText = `background: #C4C4C4`
+        
+        document.querySelector('.fday-block3-4-card-1').style.cssText = `opacity: 1`
+        document.querySelector('.fday-block3-4-card-2').style.cssText = ` `
+        document.querySelector('.fday-block3-4-card-3').style.cssText = ` `
+        document.querySelector('.fday-block3-4-dot-1').style.cssText = `background: #000`
+        document.querySelector('.fday-block3-4-dot-2').style.cssText = `background: #C4C4C4`
+        document.querySelector('.fday-block3-4-dot-3').style.cssText = `background: #C4C4C4`
     } else if (quoteNum === 2) {
         document.querySelector('.fday-block2-3-card-1').style.cssText = ` `
         document.querySelector('.fday-block2-3-card-2').style.cssText = `opacity: 1`
@@ -43,6 +72,13 @@ let quotefunction= function() {
         document.querySelector('.fday-block2-3-dot-1').style.cssText = `background: #C4C4C4`
         document.querySelector('.fday-block2-3-dot-2').style.cssText = `background: #000`
         document.querySelector('.fday-block2-3-dot-3').style.cssText = `background: #C4C4C4`
+
+        document.querySelector('.fday-block3-4-card-1').style.cssText = ` `
+        document.querySelector('.fday-block3-4-card-2').style.cssText = `opacity: 1`
+        document.querySelector('.fday-block3-4-card-3').style.cssText = ` `
+        document.querySelector('.fday-block3-4-dot-1').style.cssText = `background: #C4C4C4`
+        document.querySelector('.fday-block3-4-dot-2').style.cssText = `background: #000`
+        document.querySelector('.fday-block3-4-dot-3').style.cssText = `background: #C4C4C4`
     } else if (quoteNum === 3) {
         document.querySelector('.fday-block2-3-card-1').style.cssText = ` `
         document.querySelector('.fday-block2-3-card-2').style.cssText = ` `
@@ -50,6 +86,13 @@ let quotefunction= function() {
         document.querySelector('.fday-block2-3-dot-1').style.cssText = `background: #C4C4C4`
         document.querySelector('.fday-block2-3-dot-2').style.cssText = `background: #C4C4C4`
         document.querySelector('.fday-block2-3-dot-3').style.cssText = `background: #000`
+
+        document.querySelector('.fday-block3-4-card-1').style.cssText = ` `
+        document.querySelector('.fday-block3-4-card-2').style.cssText = ` `
+        document.querySelector('.fday-block3-4-card-3').style.cssText = `opacity: 1`
+        document.querySelector('.fday-block3-4-dot-1').style.cssText = `background: #C4C4C4`
+        document.querySelector('.fday-block3-4-dot-2').style.cssText = `background: #C4C4C4`
+        document.querySelector('.fday-block3-4-dot-3').style.cssText = `background: #000`
     }
 }
 
@@ -81,6 +124,41 @@ document.querySelector('.fday-block2-3-arrows-left').addEventListener('click', f
 })
 
 document.querySelector('.fday-block2-3-arrows-right').addEventListener('click', function() {
+    if (quoteNum === 3) {
+        quoteNum = 1
+    } else {
+        quoteNum += 1
+    }
+
+    quotefunction()
+})
+
+document.querySelector('.fday-block3-4-dot-1').addEventListener('click', function() {
+    quoteNum = 1;
+    quotefunction() ;
+})
+
+document.querySelector('.fday-block3-4-dot-2').addEventListener('click', function() {
+    quoteNum = 2;
+    quotefunction();
+})
+
+document.querySelector('.fday-block3-4-dot-3').addEventListener('click', function() {
+    quoteNum = 3;
+    quotefunction();
+})
+
+document.querySelector('.fday-block3-4-arrows-left').addEventListener('click', function() {
+    if (quoteNum === 1) {
+        quoteNum = 3
+    } else {
+        quoteNum -= 1
+    }
+
+    quotefunction()
+})
+
+document.querySelector('.fday-block3-4-arrows-right').addEventListener('click', function() {
     if (quoteNum === 3) {
         quoteNum = 1
     } else {
